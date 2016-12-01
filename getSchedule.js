71df64ada3de3c849ed4088155c8c9a0ee77e2ef
Dominik Neise <neised@phys.ethz.ch>
2016-12-01 21:14:52 +0100

    break out a few functions

--- getSchedule.js
+++ getSchedule.js
@@ -1,5 +1,8 @@
-function getSchedule()
-{
+
+function get_measurementName_by_key(){
+    // Connect to database
+    var db = new Database($['schedule-database']);
+
     // List of all available measurement types (see also Observation_class.js)
     measurementName_by_key = {}
     db.query("SELECT * from MeasurementType").forEach(
@@ -7,25 +10,34 @@ function getSchedule()
             measurementName_by_key[row.fMeasurementTypeKey] = row.fMeasurementTypeName.toUpperCase();
         }
     );
+    db.close();
+    return measurementName_by_key;
+}
+
+function get_sources_by_key(){
+    // get all sources from database and make a map by key from the list.
+    var db = new Database($['schedule-database']);
+
+    var sources_by_key = {}
+    db.query("SELECT * from Source").forEach(
+        function(source_row){
+            sources_by_key[source_row.fSourceKEY] = source_row.fSourceName
+        }
+    );
+    db.close();
+    return sources_by_key;
+}
+
+function getSchedule()
+{
+    var measurementName_by_key = get_measurementName_by_key();
+    var sources_by_key = get_sources_by_key();
 
     // Because Main.js could start a new observations just in the moment between 'now'
     // and entering the new data in the database, we have to use the unique id
     // in Main.js to check if the current observation should be changed (and sub resetted)
     var start = new Date();
     start = new Date(start.getTime()-10*3600000);
-
-    // ----------------------------------------------------------------------
-
-    // Connect to database
-    var db = new Database($['schedule-database']);
-
-    // get all sources from database and make a map by key from the list.
-    sources_by_key = {}
-    db.query("SELECT * from Source").forEach(
-        function(source_row){
-            sources_by_key[source_row.fSourceKEY] = source_row.fSourceName
-        }
-    );
 
 
     // Get the current schedule
