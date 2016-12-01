ee2748d066e4e573eaea07b12ebb43f5e7525ca1
Dominik Neise <neised@phys.ethz.ch>
2016-12-01 16:33:00 +0100

    break out the math stuff - Func ... of course .. because its functions

--- Main.js
+++ Main.js
@@ -30,6 +30,9 @@ var irq;
 include('scripts/irq_setting_functions.js');
 
 
+include('scripts/Func.js');
+
+
 dim.log("Start: "+__FILE__+" ["+__DATE__+"]");
 
 // This should be set in dimctrl.rc as JavaScript.schedule-database.
@@ -65,31 +68,6 @@ doCheckClockConditioner();
 // (This is necessary after each power cycle)
 // ----------------------------------------------------------------
 
-
-var Func = function() { };
-Func.sum = function(a, b) { return a+b; }
-Func.sq  = function(a, b) { return Math.sqrt(a*a + b*b); }
-Func.min = function(a, b) { return Math.min(a, b); }
-Func.max = function(a, b) { return Math.max(a, b); }
-Func.avg = function(arr)  { return arr.reduce(Func.Sum, 0)/arr.length; }
-Func.stat = function(arr, func)
-{
-    if (arr.length==0)
-        return undefined;
-
-    var sum = 0;
-    var sq  = 0;
-    var cnt = 0;
-    var min = arr[0];
-    var max = arr[0];
-    arr.forEach(function(val, idx) { sum+=val; sq+=val*val; if (val>max) max=val; if (val<min) min=val; if (func && func(val, idx)) cnt++ });
-    sum /= arr.length;
-    sq  /= arr.length;
-
-    return { avg:sum, rms:Math.sqrt(sq-sum*sum), min:min, max:max, count:cnt };
-}
-
-// ===================================================================
 
 console.out(("\n%78s".$("")).replace(/ /g, "="));
 
