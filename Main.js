cf0edc44682af5eaeaa7e524bd543b7ba0f66961
Dominik Neise <neised@phys.ethz.ch>
2016-12-01 13:02:04 +0100

    forgot to add this in commit before

--- Main.js
+++ Main.js
@@ -20,6 +20,7 @@ include('scripts/Hist2D.js');
 include('scripts/takeRun.js');
 include("scripts/handleDriveArmed.js");
 include('scripts/doDrsCalibration.js');
+include('scripts/lid_functions.js');
 
 dim.log("Start: "+__FILE__+" ["+__DATE__+"]");
 
@@ -33,90 +34,8 @@ if (!$['schedule-database'])
     throw new Error("Environment 'schedule-database' not set!");
 
 
-// ================================================================
-//  Code related to the schedule
-// ================================================================
-
 var observations = [ ];
-
-// ================================================================
-//  Code to perform the DRS calib sequence
-// ================================================================
-
 var irq;
-
-
-
-// ================================================================
-//  Code related to the lid
-// ================================================================
-
-function OpenLid()
-{
-    /*
-    while (Sun.horizon(-13).isUp)
-    {
-        var now = new Date();
-        var minutes_until_sunset = (Sun.horizon(-13).set - now)/60000;
-        console.out(now.toUTCString()+": Sun above FACT-horizon, lid cannot be opened: sleeping 1min, remaining %.1fmin".$(minutes_until_sunset));
-        v8.sleep(60000);
-    }*/
-
-    var isClosed = dim.state("LID_CONTROL").name=="Closed";
-    var isInconsistent = dim.state("LID_CONTROL").name=="Inconsistent";
-
-    var tm = new Date();
-
-    // Wait for lid to be open
-    if (isClosed || isInconsistent)
-    {
-        dim.log("Opening lid");
-        dim.send("LID_CONTROL/OPEN");
-
-        dim.log("Turning off IR camera LEDs...");
-
-        var cam = new Curl("fact@cam/cgi-bin/user/Config.cgi");
-        cam.data.push("action=set");
-        cam.data.push("Camera.System.Title=Camera1");
-        cam.data.push("Camera.General.IRControl.Value=2");
-        cam.data.push("Camera.System.Display=ALL");
-        cam.data.push("Camera.Environment=OUTDOOR");
-        var ret = cam.send();
-        dim.log("Camera response: "+ret.data.replace(/\n/g,"/")+" ["+ret.rc+"]");
-    }
-    //dim.wait("LID_CONTROL", "Open", 30000);
-    v8.timeout(30000, function() { if (dim.state("LID_CONTROL").name=="Open" || dim.state("LID_CONTROL").name=="PowerProblem") return true; });
-
-    if (isClosed || isInconsistent)
-        dim.log("Lid open [%.1fs]".$((new Date()-tm)/1000));
-}
-
-function CloseLid()
-{
-    var isOpen = dim.state("LID_CONTROL").name=="Open";
-    var isPowerProblem = dim.state("LID_CONTROL").name=="PowerProblem";
-
-    var tm = new Date();
-
-    // Wait for lid to be open
-    if (isOpen || isPowerProblem)
-    {
-        if (dim.state("FTM_CONTROL").name=="TriggerOn")
-        {
-            dim.send("FTM_CONTROL/STOP_TRIGGER");
-            dim.wait("FTM_CONTROL", "Valid", 3000);
-        }
-
-        dim.log("Closing lid.");
-        dim.send("LID_CONTROL/CLOSE");
-    }
-    v8.timeout(30000, function() { if (dim.state("LID_CONTROL").name=="Closed" || dim.state("LID_CONTROL").name=="Inconsistent") return true; });
-    //dim.wait("LID_CONTROL", "Closed", 30000);
-    //dim.wait("LID_CONTROL", "Inconsistent", 30000);
-
-    if (isOpen || isPowerProblem)
-        dim.log("Lid closed [%.1fs]".$((new Date()-tm)/1000));
-}
 
 // ================================================================
 //  Interrupt data taking in case of high currents
